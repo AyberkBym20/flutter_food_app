31430014d95ff0fa64fdbd06508fbfa529c077f4
Ayberk Bayam <bayam20cp@gmail.com>
2026-03-13 20:34:05 +0300

    feature(models): A model class demo was created

---
+++ lib/models/meal_models.dart
@@ -0,0 +1,13 @@
+class MealModels {
+  final int id;
+  final String name;
+  final String imageUrl;
+  final int price;
+
+  MealModels({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+    required this.price,
+  });
+}
