81478c1e587192e6581578e0a92ade60ac8a6d7e
Ayberk Bayam <bayam20cp@gmail.com>
2026-03-13 20:23:51 +0300

    feat: main connections have been made

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_food_app/features/meals/presentation/meals_page.dart';
+import 'package:flutter_food_app/features/meals/meals_page.dart';
 
 void main() {
   runApp(const MyApp());
