fc85032cee57e45b27a57afd4670f9ab6b25b687
Ayberk Bayam <bayam20cp@gmail.com>
2026-03-14 15:45:49 +0300

    feat: a connection has been established with  maine multi-bloc providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_food_app/features/meals/meals_page.dart';
+import 'package:flutter_food_app/features/meals/presentation/cubit/meals_cubit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,10 +11,13 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MealsPage(),
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => MealsCubit())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+        home: const MealsPage(),
+      ),
     );
   }
 }
