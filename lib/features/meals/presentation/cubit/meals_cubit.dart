4220be7e13c207f315eb8c5e453b7bd815de9c01
Ayberk Bayam <bayam20cp@gmail.com>
2026-03-14 16:37:29 +0300

    feat: the connection between cubit and repo has been fixed

--- lib/features/meals/presentation/cubit/meals_cubit.dart
+++ lib/features/meals/presentation/cubit/meals_cubit.dart
@@ -3,11 +3,16 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_food_app/repositories/mealdao_repo.dart';
 
 class MealsCubit extends Cubit<List<MealModels>> {
-  MealsCubit() : super(<MealModels>[]);
+  final MealDaoRepo repo = MealDaoRepo();
 
-  var mealRepo = MealDaoRepo();
-  Future<void> mealsUpdate() async {
-    var list = await mealRepo.mealsUpdate();
+  MealsCubit() : super([]) {
+    getMeals();
+  }
+  Future<void> getMeals() async {
+    print('1. Fonksiyon tetiklendi.');
+    var list = await repo.mealsUpdate();
+    print('2.Repo dan ${list.length}. tane veri geldi');
+
     emit(list);
   }
 }
--- lib/repositories/mealdao_repo.dart
+++ lib/repositories/mealdao_repo.dart
@@ -4,14 +4,36 @@ class MealDaoRepo {
   Future<List<MealModels>> mealsUpdate() async {
     var list = <MealModels>[];
 
-    var meal1 = MealModels(id: 1, name: 'Ayran', imageUrl: '', price: 25);
+    var meal1 = MealModels(
+      id: 1,
+      name: 'Ayran',
+      imageUrl: 'image/ayran.jpg',
+      price: 25,
+    );
 
-    var meal2 = MealModels(id: 2, name: 'Köfte', imageUrl: '', price: 50);
+    var meal2 = MealModels(
+      id: 2,
+      name: 'Köfte',
+      imageUrl: 'image/köfte.png',
+      price: 50,
+    );
 
-    var meal3 = MealModels(id: 3, name: 'Pilav', imageUrl: '', price: 75);
+    var meal3 = MealModels(
+      id: 3,
+      name: 'Pilav',
+      imageUrl: 'image/pilav.jpg',
+      price: 75,
+    );
+    var meal4 = MealModels(
+      id: 4,
+      name: 'Pilav',
+      imageUrl: 'image/pilav.jpg',
+      price: 100,
+    );
     list.add(meal1);
     list.add(meal2);
     list.add(meal3);
+    list.add(meal4);
 
     return list;
   }
