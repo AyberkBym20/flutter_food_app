03046c5b6e358e8535d70167d89693e16ea654f1
Ayberk Bayam <bayam20cp@gmail.com>
2026-03-14 15:43:50 +0300

    feat: a cubit repo link has been created

--- lib/features/meals/presentation/cubit/meals_cubit.dart
+++ lib/features/meals/presentation/cubit/meals_cubit.dart
@@ -1,4 +1,13 @@
 import 'package:flutter_food_app/models/meal_models.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_food_app/repositories/mealdao_repo.dart';
 
-class MealsCubit extends Cubit<List<MealModels>> {}
+class MealsCubit extends Cubit<List<MealModels>> {
+  MealsCubit() : super(<MealModels>[]);
+
+  var mealRepo = MealDaoRepo();
+  Future<void> mealsUpdate() async {
+    var list = await mealRepo.mealsUpdate();
+    emit(list);
+  }
+}
