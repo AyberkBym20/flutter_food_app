cc3cdcc9fe043282bc104ef5a6b662eed087d4b5
Ayberk Bayam <bayam20cp@gmail.com>
2026-03-16 16:45:29 +0300

    feat: the meal data response class has been completed

--- lib/features/meals/presentation/data/meal_remote_data_source.dart
+++ lib/features/meals/presentation/data/meal_remote_data_source.dart
@@ -2,5 +2,6 @@ import 'package:flutter_food_app/models/meal_models.dart';
 
 class MealRemoteDataSource {
   List<MealModels> meal;
-  MealRemoteDataSource({required this.meal});
+  int success;
+  MealRemoteDataSource({required this.meal, required this.success});
 }
