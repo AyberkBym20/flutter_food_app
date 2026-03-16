d55a296621f3e0a5468bdcf1cbb91721ce32bce2
Ayberk Bayam <bayam20cp@gmail.com>
2026-03-16 17:10:04 +0300

    feat: a list was read from the answer class

--- lib/features/meals/presentation/data/meal_remote_data_source.dart
+++ lib/features/meals/presentation/data/meal_remote_data_source.dart
@@ -4,4 +4,14 @@ class MealRemoteDataSource {
   List<MealModels> meal;
   int success;
   MealRemoteDataSource({required this.meal, required this.success});
+
+  factory MealRemoteDataSource.fromJson(Map<String, dynamic> json) {
+    var jsonArray = json['yemekler'] as List;
+    int success = json['success'] as int;
+    var meals = jsonArray
+        .map((jsonArrayObject) => MealModels.fromJson(jsonArrayObject))
+        .toList();
+
+    return MealRemoteDataSource(meal: meals, success: success);
+  }
 }
