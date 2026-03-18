e29c73fbadbeec78e19fddea1b6be82bbe6c1ea7
Ayberk Bayam <bayam20cp@gmail.com>
2026-03-18 13:29:37 +0300

    Refactor: the repo class has been uptaded

--- lib/repositories/mealdao_repo.dart
+++ lib/repositories/mealdao_repo.dart
@@ -1,17 +1,45 @@
 import 'dart:convert';
-
 import 'package:dio/dio.dart';
 import 'package:flutter_food_app/features/meals/presentation/data/meal_remote_data_source.dart';
 import 'package:flutter_food_app/models/meal_models.dart';
 
 class MealDaoRepo {
-  List<MealModels> parseMeals(String answer) {
-    return MealRemoteDataSource.fromJson(json.decode(answer)).meal;
+  final Dio _dio = Dio();
+
+  List<MealModels> parseMeals(dynamic answer) {
+    return MealRemoteDataSource.fromJson(answer).meal;
   }
 
   Future<List<MealModels>> mealsUpdate() async {
-    var url = "http://kasimadalan.pe.hu/yemekler/tumYemekleriGetir.php";
-    var answer = await Dio().get(url);
-    return parseMeals(answer.data.toString());
+    var url =
+        "http://kasimadalan.pe.hu/yemekler/tumYemekleriGetir.php"; // [cite: 84]
+    var answer = await _dio.get(url);
+    return parseMeals(answer.data);
+  }
+
+  Future<void> save({
+    required String foodName,
+    required String foodImage,
+    required int foodPrice,
+    required int foodOrderAmount,
+    required String userName,
+  }) async {
+    var url =
+        "http://kasimadalan.pe.hu/yemekler/sepeteYemekEkle.php"; // [cite: 86]
+
+    var data = {
+      "yemek_adi": foodName,
+      "yemek_resim_adi": foodImage,
+      "yemek_fiyat": foodPrice,
+      "yemek_siparis_adet": foodOrderAmount,
+      "kullanici_adi": userName,
+    };
+
+    try {
+      // Veri gönderirken FormData.fromMap kullanmak Dio için doğru yaklaşımdır.
+      await _dio.post(url, data: FormData.fromMap(data));
+    } catch (e) {
+      print("Sepete ekleme hatası: $e");
+    }
   }
 }
