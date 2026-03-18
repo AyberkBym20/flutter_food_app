f72f18ef2899ef3f0945740732336c758007d2d5
Ayberk Bayam <bayam20cp@gmail.com>
2026-03-18 14:27:01 +0300

    feat: the cubit connection has been established

--- lib/features/meals/presentation/cubit/detail_cubit.dart
+++ lib/features/meals/presentation/cubit/detail_cubit.dart
@@ -1,1 +1,29 @@
-class DetailCubit {}
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_food_app/repositories/mealdao_repo.dart';
+
+class DetailCubit extends Cubit<int> {
+  DetailCubit() : super(1);
+  var repo = MealDaoRepo();
+
+  void increment() {
+    emit(state + 1);
+  }
+
+  void decrement() {
+    emit(state + 1);
+  }
+
+  Future<void> addToCart({
+    required String foodName,
+    required String foodImage,
+    required int foodPrice,
+  }) async {
+    await repo.save(
+      foodName: foodName,
+      foodImage: foodImage,
+      foodPrice: foodPrice,
+      foodOrderAmount: state,
+      userName: 'ayberk_dev',
+    );
+  }
+}
