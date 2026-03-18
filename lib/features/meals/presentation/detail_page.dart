cf7209f5d8b855b621d5ef5bd5fad471a2e78026
Ayberk Bayam <bayam20cp@gmail.com>
2026-03-18 14:44:53 +0300

    feature: the bussines logic for the detail page has been implemented

--- lib/features/meals/presentation/detail_page.dart
+++ lib/features/meals/presentation/detail_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_food_app/features/meals/presentation/cubit/detail_cubit.dart';
+import 'package:flutter_food_app/features/meals/presentation/cubit/meals_cubit.dart';
 
 import 'package:flutter_food_app/models/meal_models.dart';
 
@@ -22,7 +25,49 @@ class _DetailPageState extends State<DetailPage> {
             Image.network(widget.model.imageUrl),
             Text(widget.model.name),
             Text(widget.model.price.toString()),
-            ElevatedButton(onPressed: () {}, child: const Text('Add to cart')),
+            Row(
+              mainAxisAlignment: .spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<DetailCubit>().increment();
+                  },
+                  child: const Icon(Icons.add),
+                ),
+                BlocBuilder<DetailCubit, int>(
+                  builder: (context, state) {
+                    return Text('$state', style: const TextStyle(fontSize: 24));
+                  },
+                ),
+
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<DetailCubit>().decrement();
+                  },
+                  child: const Icon(Icons.remove),
+                ),
+              ],
+            ),
+
+            ElevatedButton(
+              onPressed: () {
+                context
+                    .read<DetailCubit>()
+                    .addToCart(
+                      foodName: widget.model.name,
+                      foodImage: widget.model.imageUrl,
+                      foodPrice: widget.model.price,
+                    )
+                    .then((value) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('${widget.model.name} sepete eklendi'),
+                        ),
+                      );
+                    });
+              },
+              child: const Text('Add to cart'),
+            ),
           ],
         ),
       ),
